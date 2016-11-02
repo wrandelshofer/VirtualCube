80cb3b2d6a19ae76e19ce28bb7181fc7537459e9
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2016-11-02 11:57:59 +0100

    Adds NAME_PART_MAP.

--- public_html/src/lib/RubiksCube.js
+++ public_html/src/lib/RubiksCube.js
@@ -1250,6 +1250,43 @@ function (Cube) {
       [0, 2, 1]
     ]
   ];
+  
+// Construct the name to par map.
+  let cornerParts = ["urf", "dfr", "ubr", "drb", "ulb", "dbl", "ufl", "dlf"];
+  let edgeParts=[ "ur","rf","dr","bu","rb","bd","ul","lb","dl","fu","lf","fd"];
+  let sideParts=[ "r","u","f","l","d","b"];
+  let partMap = {center: 8};
+  for (let i = 0; i < cornerParts.length; i++) {
+    let name = cornerParts[i];
+    let key1 = name.charAt(0) + name.charAt(1) + name.charAt(2);
+    let key2 = name.charAt(0) + name.charAt(2) + name.charAt(1);
+    let key3 = name.charAt(1) + name.charAt(0) + name.charAt(2);
+    let key4 = name.charAt(1) + name.charAt(2) + name.charAt(0);
+    let key5 = name.charAt(2) + name.charAt(0) + name.charAt(1);
+    let key6 = name.charAt(2) + name.charAt(1) + name.charAt(0);
+    partMap[key1] = i;
+    partMap[key2] = i;
+    partMap[key3] = i;
+    partMap[key4] = i;
+    partMap[key5] = i;
+    partMap[key6] = i;
+  }
+  for (let i = 0; i < edgeParts.length; i++) {
+    let name = edgeParts[i];
+    let key1 = name.charAt(0) + name.charAt(1);
+    let key2 = name.charAt(1) + name.charAt(0);
+    partMap[key1] = i+8;
+    partMap[key2] = i+8;
+  }
+  for (let i = 0; i < sideParts.length; i++) {
+    let name = sideParts[i];
+    let key1 = name;
+    partMap[key1] = i+8+12;
+  }
+/**
+ * Maps the name of a part to its part index.
+ */
+RubiksCube.prototype.NAME_PART_MAP = partMap;
 
 
 
