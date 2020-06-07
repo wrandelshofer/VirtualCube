834763b1a5e534799463f32a8487de82538d0271
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2020-06-07 19:19:35 +0200

    Fixes colorlist parsing.

--- public_html/src/lib/CubeAttributes.mjs
+++ public_html/src/lib/CubeAttributes.mjs
@@ -14,57 +14,57 @@
  */
 class CubeAttributes {
   constructor(partCount, stickerCount, stickerCounts) {
-  this. alpha = (-25 / 180.0 * Math.PI);
-  this. beta = (45  / 180.0 * Math.PI);
+    this. alpha = (-25 / 180.0 * Math.PI);
+    this. beta = (45  / 180.0 * Math.PI);
 
-  this.partsVisible = new Array(partCount);//boolean
-  this.partsFillColor = new Array(partCount);//[r,g,b,a]
-  this.partsPhong = new Array(partCount);//[ambient, diffuse, specular, shininess]
-  this.stickersVisible = new Array(stickerCount);//boolean
-  this.stickersFillColor = new Array(stickerCount);//[r,g,b,a]
-  this.stickersPhong = new Array(stickerCount);//[ambient, diffuse, specular, shininess]
-  this.stickerCounts = stickerCounts;//integer array for each face
-  this.partExplosion = new Array(partCount);//float
-  this.stickerExplosion = new Array(stickerCount);//float
-  this.xRot=-25;
-  this.yRot=-45;
-  this.scaleFactor=1.0;
-  this.explosionFactor=0;
-  this.developmentFactor=0; // range [0,1]
+    this.partsVisible = new Array(partCount);//boolean
+    this.partsFillColor = new Array(partCount);//[r,g,b,a]
+    this.partsPhong = new Array(partCount);//[ambient, diffuse, specular, shininess]
+    this.stickersVisible = new Array(stickerCount);//boolean
+    this.stickersFillColor = new Array(stickerCount);//[r,g,b,a]
+    this.stickersPhong = new Array(stickerCount);//[ambient, diffuse, specular, shininess]
+    this.stickerCounts = stickerCounts;//integer array for each face
+    this.partExplosion = new Array(partCount);//float
+    this.stickerExplosion = new Array(stickerCount);//float
+    this.xRot=-25;
+    this.yRot=-45;
+    this.scaleFactor=1.0;
+    this.explosionFactor=0;
+    this.developmentFactor=0; // range [0,1]
 
-  this.stickersImageURL=null;
-  this.stickersImageVisible=true;
+    this.stickersImageURL=null;
+    this.stickersImageVisible=true;
 
-  // The twist duration of the cube.
-  this.twistDuration=500;
+    // The twist duration of the cube.
+    this.twistDuration=500;
 
-  // The twist duration that will be set on twistDuration when the user
-  // performs an twist operation.
-  this.userTwistDuration=500;
+    // The twist duration that will be set on twistDuration when the user
+    // performs an twist operation.
+    this.userTwistDuration=500;
 
-  // The twist duration that will be set on twistDuration when the program
-  // performs a scramble operation.
-  this.scrambleTwistDuration=200;
+    // The twist duration that will be set on twistDuration when the program
+    // performs a scramble operation.
+    this.scrambleTwistDuration=200;
 
-  this.backgroundColor=[0, 0, 0, 0]; //[r,g,b,a]
+    this.backgroundColor=[0, 0, 0, 0]; //[r,g,b,a]
 
-  for (let i=0;i<partCount;i++) {
-    this.partsVisible[i]=true;
-    this.stickersVisible[i]=true;
-    this.partExplosion[i]=0;
-  }
+    for (let i=0;i<partCount;i++) {
+      this.partsVisible[i]=true;
+      this.stickersVisible[i]=true;
+      this.partExplosion[i]=0;
+    }
 
-  // The following values must be initialized explicitly
-  this.faceCount=stickerCounts.length;
-  this.stickerOffsets=new Array(stickerCounts);
-  this.stickerOffsets[0]=0;
-  for (let i=0;i<stickerCounts-1;i++) {
-    this.stickerOffsets[i+1]=this.stickerOffsets[i]+stickerCounts[i];
-  }
+    // The following values must be initialized explicitly
+    this.faceCount=stickerCounts.length;
+    this.stickerOffsets=new Array(stickerCount);
+    this.stickerOffsets[0]=0;
+    for (let i=0;i<stickerCounts.length-1;i++) {
+      this.stickerOffsets[i+1]=this.stickerOffsets[i]+stickerCounts[i];
+    }
   }
 
   getFaceCount() {
-  return this.faceCount;
+    return this.faceCount;
   }
   getStickerOffset(face) {
   return this.stickerOffsets[face];
